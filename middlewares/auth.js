2c3f216e147010cdee410e81a5f66b42b04f68f7
CRIMINAL <butani.hardik53@gmail.com>
2025-08-31 17:21:25 +0530

    get token from request

--- middlewares/auth.js
+++ middlewares/auth.js
@@ -2,12 +2,11 @@ import { decodeToken, createError } from "../lib/utils";
 import User from "../models/User.js";
 export async function protect(req, res, next) {
     try {
-        const token = "";
+        const token = req.headers.authorization;
+        if (!token) return next(createError("Not authorized, token failed", 401));
         const decode = decodeToken(toke);
         const user = await User.findById(decode.id).select('-password');
-        if (!user) {
-            return next(createError("Not authorized, token failed", 401));
-        }
+        if (!user) return next(createError("Not authorized, token failed", 401));
         req.user = user;
         next();
     } catch (error) {
